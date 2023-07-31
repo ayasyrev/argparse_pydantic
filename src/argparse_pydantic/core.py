ce8794999053901fa22c566bee730acfb7591419
ayasyrev <a.yasyrev@gmail.com>
2023-07-31 17:59:36 +0300

    union types fix

--- src/argparse_pydantic/core.py
+++ src/argparse_pydantic/core.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import argparse
+import sys
 from typing import Any, Iterable, Optional, Sequence, Type, Union
 
 from pydantic import BaseModel
@@ -29,6 +30,16 @@ ARG_KEYWORDS = (
     "flag",
     "positional",
 )
+
+
+if sys.version_info < (3, 10):  # pragma: no cover
+    def is_union(tp: type[Any] | None) -> bool:
+        return get_origin(tp) is Union
+else:
+    from types import UnionType
+
+    def is_union(tp: type[Any] | None) -> bool:
+        return get_origin(tp) is Union or isinstance(tp, UnionType)
 
 
 def argument_kwargs(
@@ -68,7 +79,7 @@ def argument_kwargs(
 def get_field_type(field_info: FieldInfo) -> Type:
     """get field type, convert to base type."""
     field_type = field_info.annotation
-    if get_origin(field_type) is Union:
+    if is_union(field_type):
         return get_args(field_type)[0]
     return field_type
 
