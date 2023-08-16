0e737a473b7622c46cfa838ac1180e51bf95f660
ayasyrev <a.yasyrev@gmail.com>
2023-08-16 16:47:51 +0300

    rename get_examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,10 +1,10 @@
 import pytest
 
-from cli_result.core import Cfg, get_examples_names, run_check_example
+from cli_result.core import Cfg, get_examples, run_check_example
 
 
 cfg = Cfg(examples_path="examples")
-examples = get_examples_names(cfg=cfg)
+examples = get_examples(cfg=cfg)
 
 
 @pytest.mark.parametrize("example_name, file_list", examples)
