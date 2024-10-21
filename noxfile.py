c29f5c1039b26403074092d85258305c332e5bce
ayasyrev <a.yasyrev@gmail.com>
2024-10-21 11:52:01 +0000

    fix nox

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,12 @@
 import nox
 
 
+nox.options.default_venv_backend = "uv|virtualenv"
+nox.options.reuse_existing_virtualenvs = True
+
+
 @nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
 def tests(session: nox.Session) -> None:
     args = session.posargs or ["--cov"]
-    session.install("uv")
-    session.run("uv", "pip", "install", ".[test]")
+    session.install("-e", ".[test]")
     session.run("pytest", *args)
